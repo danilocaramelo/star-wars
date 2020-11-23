9400790e23965ad80f343424fe7ead0e5cc23c0e
Danilo Caramelo <danilo.caramelogit@gmail.com>
2020-11-23 15:05:31 -0300

    feat: increase responsive layout in Movies and 50% in planets

--- src/components/PlanetCard/PlanetCard.jsx
+++ src/components/PlanetCard/PlanetCard.jsx
@@ -1,27 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./planetcard.css";
 
 export default function PlanetCard(props) {
 
-    const [isShow, setIsShow] = useState(false);
-
     return(
         <div className={props.value.className}>
             <img
-                onMouseEnter={() => setIsShow(true)}
-                onMouseLeave={() => setIsShow(false)}
                 className={"planetcard-image"}
                 src={props.value.image}
                 alt={props.value.name + ", star wars planet"}
             />
-            {isShow && (
-                <div className={"planetcard-information"}>
-                    <h1 className={"planetcard-title"}>{props.value.name}</h1>
-                    <p className={"planetcard-text"}>Climate: {props.value.climate}</p>
-                    <p className={"planetcard-text"}>Diameter: {props.value.diameter}</p>
-                    <p className={"planetcard-text"}>Gravity: {props.value.gravity}</p>
-                </div>
-            )}
+            <div className={"planetcard-information"}>
+                <h1 className={"planetcard-title"}>{props.value.name}</h1>
+                <p className={"planetcard-text"}>Climate: {props.value.climate}</p>
+                <p className={"planetcard-text"}>Diameter: {props.value.diameter}</p>
+                <p className={"planetcard-text"}>Gravity: {props.value.gravity}</p>
+            </div>
         </div>
     )
 }
