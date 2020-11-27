4a272aa0066b56b1960b5fe085e9e98d172eadaa
Danilo Caramelo <danilo.caramelogit@gmail.com>
2020-11-27 16:40:18 -0300

    feat: improvment movies path

---
+++ src/views/introduction.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Animation from "../components/Animation/Animation";
+
+export default function Introduction() {
+    return(
+        <>
+            <Animation/>
+        </>
+    )
+}
