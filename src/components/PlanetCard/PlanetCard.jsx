eb75ca55bdb9bfe099b0196d1349b979f6926f2a
Danilo Caramelo <danilo.caramelogit@gmail.com>
2020-11-27 18:25:47 -0300

    fix: cleaned planetcard

--- src/components/PlanetCard/PlanetCard.jsx
+++ src/components/PlanetCard/PlanetCard.jsx
@@ -6,15 +6,15 @@ export default function PlanetCard(props) {
     return(
         <div className={props.value.className}>
             <img
-                className={"planetcard-image"}
+                className={"planetcard__image"}
                 src={props.value.image}
                 alt={props.value.name + ", star wars planet"}
             />
-            <div className={"planetcard-information"}>
-                <h1 className={"planetcard-title"}>{props.value.name}</h1>
-                <p className={"planetcard-text"}>Climate: {props.value.climate}</p>
-                <p className={"planetcard-text"}>Diameter: {props.value.diameter}</p>
-                <p className={"planetcard-text"}>Gravity: {props.value.gravity}</p>
+            <div className={"planetcard__information"}>
+                <h1 className={"planetcard__title"}>{props.value.name}</h1>
+                <p className={"planetcard__text"}>Climate: {props.value.climate}</p>
+                <p className={"planetcard__text"}>Diameter: {props.value.diameter}</p>
+                <p className={"planetcard__text"}>Gravity: {props.value.gravity}</p>
             </div>
         </div>
     )
